dda15b2f4d1705c5b1fd072655394da65df420cf
Becn <becn2772@gmail.com>
2023-03-02 19:22:45 -0500

    mi primer commit

---
+++ routes/auth.routes.js
@@ -0,0 +1,35 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { signup, login } = require('../controllers/auth.controller');
+const { validIfExistUserEmail } = require('../middlewares/user.middleware');
+const { validateFields } = require('../middlewares/validateField.middleware');
+
+const router = Router();
+
+router.post(
+  '/signup',
+  [
+    check('name', 'The name must be mandatory').not().isEmpty(),
+    check('email', 'The email must be mandatory').not().isEmpty(),
+    check('email', 'The email must be a correct format').isEmail(),
+    check('password', 'The password must be mandatory').not().isEmpty(),
+    validateFields,
+    validIfExistUserEmail,
+  ],
+  signup
+);
+
+router.post(
+  '/login',
+  [
+    check('email', 'The email must be mandatory').not().isEmpty(),
+    check('email', 'The email must be a correct format').isEmail(),
+    check('password', 'The password must be mandatory').not().isEmpty(),
+    validateFields,
+  ],
+  login
+);
+
+module.exports = {
+  authRouter: router,
+};
